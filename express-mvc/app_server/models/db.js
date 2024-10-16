cc7197e04623bab9442e3db2e87b5e761c9afdaf
msyakira <muthiasyakirah4@gmail.com>
2024-10-16 09:38:45 +0700

    up

---
+++ express-mvc/app_server/models/db.js
@@ -0,0 +1,15 @@
+let mongoose = require("mongoose");
+let dbURI = "mongodb://localhost:27017/pawII-si5c";
+
+mongoose.connect(dbURI, {
+    //useNewUrlParser: true
+});
+mongoose.connection.on("connected", () => {
+    console.log("Connected to MongoDB") ;
+});
+mongoose.connection.on("error", (error) => {
+    console.log("Connection Error : " + error);
+});
+mongoose.connection.on("disconnected", () => {
+    console.log("Disconnected from MongoDB");
+});
